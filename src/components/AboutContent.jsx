49ded61fb9059cead0e270ab18231644e9316baf
mitali1411 <mittali.rasaniya1411@gmail.com>
2024-10-25 00:02:08 +0530

    Pages update

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -4,8 +4,8 @@ const AboutContent = () => {
   return (
     <div className='container-fluid p-5 d-flex align-items-center justify-content-center flex-column' id='about'>
         <h2 className='text-center text-dark'>ABOUT US</h2>
-        <p className='mt-4 text-body-secondary'>At SEHAT KI PAATHSHALA, We are a dedicated team of health enthusiasts, nutritionists, and wellness advocates committed to sharing evidence-based insights and practical guidance. Our focus is on educating our community about the benefits of natural foods, the importance of a balanced diet, and how lifestyle choices can significantly impact overall well-being.</p>
-        <p className='text-body-secondary'>Join us on this journey towards a healthier future, where we prioritize nourishing the body with nature’s best offerings. Together, we can cultivate a vibrant, healthy lifestyle that promotes wellness from within.</p>
+        <p className='text-body-secondary'>At SEHAT KI PAATHSHALA, We are a dedicated team of health enthusiasts, nutritionists, and wellness advocates committed to sharing evidence-based insights and practical guidance. Our focus is on educating our community about the benefits of natural foods, the importance of a balanced diet, and how lifestyle choices can significantly impact overall well-being.</p>
+        <p className='text-body-secondary mt-2'>Join us on this journey towards a healthier future, where we prioritize nourishing the body with nature’s best offerings. Together, we can cultivate a vibrant, healthy lifestyle that promotes wellness from within.</p>
     </div>
   )
 }
